1a805c8c9d5f21e24a2463a7d8d8fcdd0fec7347
Ludwig Ruderstaller <lr@cwd.at>
2017-09-24 10:49:56 +0200

    Release 0.3.2

--- src/Domain/User/Exception/UserNotFoundException.php
+++ src/Domain/User/Exception/UserNotFoundException.php
@@ -2,7 +2,9 @@
 
 /*
  * This file is part of Application.
- * (c) 2016 cwd.at GmbH <office@cwd.at>
+ *
+ * (c) 2017 cwd.at GmbH <office@cwd.at>
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
